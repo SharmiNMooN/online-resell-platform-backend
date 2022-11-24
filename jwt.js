1f3b26f1d29c96ea0374317e0f3a17cd71b91bc1
Sharmin Moon <sharminmoon0308@gmail.com>
2022-11-24 12:09:24 +0600

    jwt middleware added

---
+++ jwt.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+module.exports = {
+  verifyJWT: (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  },
+};
